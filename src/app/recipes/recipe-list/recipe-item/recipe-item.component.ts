d4774bb2ec4653084d4ac8aa97ee1f9c1c5cc8cb
Tom (Thanh Le) <thanh.lenguyenphuoc@orientsoftware.com>
2023-01-06 15:08:51 +0700

    recipe binding data

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Recipe } from '../../recipes.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
 })
 export class RecipeItemComponent {
+  @Input() recipe!: Recipe;
+  @Output() recipeSelected = new EventEmitter();
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
